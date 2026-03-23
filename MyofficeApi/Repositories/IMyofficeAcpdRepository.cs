93323dd994ca3facc1f3e4797c01310ae4abec16
chen0902jp <chen0902jp@gmail.com>
2026-03-23 15:59:13 +0800

    feat: finalized crud api and backup file

---
+++ MyofficeApi/Repositories/IMyofficeAcpdRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyofficeApi.Models;
+
+namespace MyofficeApi.Repositories
+{
+    public interface IMyofficeAcpdRepository
+    {
+        Task<IEnumerable<MyOfficeAcpd>> GetAllAsync();
+        Task<MyOfficeAcpd?> GetByIdAsync(string id);
+        Task<string> CreateAsync(MyOfficeAcpd entity);
+        Task<bool> UpdateAsync(string id, MyOfficeAcpd entity);
+        Task<bool> DeleteAsync(string id);
+    }
+}
---
+++ MyofficeApi/Services/MyofficeAcpdService.cs
@@ -0,0 +1,25 @@
+﻿using MyofficeApi.Models;
+using MyofficeApi.Repositories;
+
+namespace MyofficeApi.Services
+{
+    public class MyofficeAcpdService : IMyofficeAcpdService
+    {
+        private readonly IMyofficeAcpdRepository _repository;
+
+        public MyofficeAcpdService(IMyofficeAcpdRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<MyOfficeAcpd>> GetAllAsync() => await _repository.GetAllAsync();
+
+        public async Task<MyOfficeAcpd?> GetByIdAsync(string id) => await _repository.GetByIdAsync(id);
+
+        public async Task<string> CreateAsync(MyOfficeAcpd entity) => await _repository.CreateAsync(entity);
+
+        public async Task<bool> UpdateAsync(string id, MyOfficeAcpd entity) => await _repository.UpdateAsync(id, entity);
+
+        public async Task<bool> DeleteAsync(string id) => await _repository.DeleteAsync(id);
+    }
+}
